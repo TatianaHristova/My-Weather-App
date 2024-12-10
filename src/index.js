570b3eaaf653a87be6ee76856c4b03c7ff77b0aa
TatianaHristova <hristova.titi@gmail.com>
2024-12-10 22:57:36 +0000

    Inject the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -56,7 +56,34 @@ function handleSubmitCity(event) {
   searchForCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-emoji">☀️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15°</strong>
+              </div>
+              <div class="weather-forecast-temperature">8°</div>
+            </div>
+          </div>
+          `;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSubmitCity);
 
 searchForCity("Paris");
+displayForecast();
